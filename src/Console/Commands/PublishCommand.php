61628954bdabb4e00173ca7db6cdf5d31af0e0cc
Abdelrahman Omran <me@omranic.com>
2017-10-29 05:23:04 +0200

    Tweak artisan command output lines

--- src/Console/Commands/PublishCommand.php
+++ src/Console/Commands/PublishCommand.php
@@ -29,7 +29,7 @@ class PublishCommand extends Command
      */
     public function handle()
     {
-        $this->warn('Publish rinvex/addresses:');
+        $this->warn($this->description);
         $this->call('vendor:publish', ['--tag' => 'rinvex-addresses-config', '--force' => $this->option('force')]);
     }
 }
