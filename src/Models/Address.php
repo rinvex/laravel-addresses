94fe3b934e8427410573d24068ee0b681e04b06c
Abdelrahman Omran <me@omranic.com>
2018-02-20 12:24:49 +0200

    Define polymorphic relationship parameters explicitly

--- src/Models/Address.php
+++ src/Models/Address.php
@@ -188,7 +188,7 @@ class Address extends Model
      */
     public function addressable(): MorphTo
     {
-        return $this->morphTo();
+        return $this->morphTo('addressable', 'addressable_type', 'addressable_id');
     }
 
     /**
