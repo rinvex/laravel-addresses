4edf2c8aeb940503a2aac7664522a457fc42862b
Abdelrahman Omran <me@omranic.com>
2018-04-09 11:51:36 +0200

    Add soft deletes

--- database/migrations/2017_03_31_005125_create_addresses_table.php
+++ database/migrations/2017_03_31_005125_create_addresses_table.php
@@ -27,6 +27,7 @@ class CreateAddressesTable extends Migration
             $table->boolean('is_billing')->default(false);
             $table->boolean('is_shipping')->default(false);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
