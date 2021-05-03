a9e0061560f8f349874b9a5a5f6b8cdfa986c4ba
Ahmed Hekal <android.com111@gmail.com>
2021-05-03 04:23:18 +0200

    Add Phone Support

--- database/migrations/2020_01_01_000001_create_addresses_table.php
+++ database/migrations/2020_01_01_000001_create_addresses_table.php
@@ -23,6 +23,7 @@ class CreateAddressesTable extends Migration
             $table->string('state')->nullable();
             $table->string('city')->nullable();
             $table->string('postal_code')->nullable();
+            $table->string('phone')->nullable();
             $table->decimal('latitude', 10, 7)->nullable();
             $table->decimal('longitude', 10, 7)->nullable();
             $table->boolean('is_primary')->default(false);
