aef0d5f60cda78d778d0e152b356267c2e387741
Abdelrahman Omran <me@omranic.com>
2021-09-30 03:12:14 +0400

    Update and rename 2020_01_01_000002_set_family_name_nullable_in_addresses_table.php to 2020_01_01_000002_alter_addresses_table_make_family_name_nullable.php

--- database/migrations/2020_01_01_000002_alter_addresses_table_make_family_name_nullable.php
+++ database/migrations/2020_01_01_000002_alter_addresses_table_make_family_name_nullable.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAddressesTable extends Migration
+class AlterAddressesTableMakeFamilyNameNullable extends Migration
 {
     public function up()
     {
